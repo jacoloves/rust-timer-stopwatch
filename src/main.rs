8e662cf4720de77b96376c80094536be0a736d9c
jacoloves <5511068t@gmail.com>
2023-01-02 15:19:06 +0900

    timer function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use std::env;
 use std::thread;
 use std::time::Duration;
 
 const MARK: [&str; 4] = ["-", "\\", "|", "/"];
+const NUMBER: [[i32; 15]; 10] = [
+    [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+];
 
 #[derive(Debug)]
 struct Displaytime {
-    second: u32,
-    minute: u32,
-    hour: u32,
+    second: i32,
+    minute: i32,
+    hour: i32,
     second_str: String,
     minute_str: String,
     hour_str: String,
@@ -58,9 +71,67 @@ impl Displaytime {
             self.hour_str = self.hour.to_string();
         }
     }
+
+    fn declement_time(&mut self) -> bool {
+        let mut finish_flg = false;
+        self.second = self.second - 1;
+        if self.second == -1 {
+            self.minute = self.minute - 1;
+            self.second = 59;
+        }
+
+        if self.minute == -1 {
+            self.hour = self.hour - 1;
+            self.minute = 59;
+        }
+
+        if self.hour == -1 {
+            finish_flg = true;
+        }
+
+        finish_flg
+    }
 }
 
-fn main() {
+fn option_select() -> (bool, bool, bool, u32) {
+    let mut timer_flg: bool = false;
+    let mut stopwatch_flg: bool = false;
+    let mut end_flg: bool = false;
+    let mut timer_minute: u32 = 0;
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        stopwatch_flg = true;
+    } else if args.len() >= 2 {
+        let option_part: String = args[1].clone();
+        if option_part.chars().nth(0).unwrap() != '-' {
+            println!("When using options, prefix the option with '-'.");
+            end_flg = true;
+        } else {
+            if option_part.chars().nth(1).unwrap() == 't' {
+                timer_flg = true;
+            } else if option_part.chars().nth(1).unwrap() == 's' {
+                stopwatch_flg = true;
+            } else {
+                println!("Only 't' or 's' can be used as an option.");
+                end_flg = true;
+            }
+        }
+
+        if timer_flg && args.len() == 3 {
+            (timer_minute, end_flg) = match args[2].trim().parse() {
+                Ok(num) => (num, false),
+                Err(_) => (0, true),
+            };
+
+            if end_flg {
+                println!("Please enter a number.");
+            }
+        }
+    }
+    (timer_flg, stopwatch_flg, end_flg, timer_minute)
+}
+
+fn stopwatch_disp() {
     let mut timedisp = Displaytime {
         second: 0,
         minute: 0,
@@ -75,5 +146,46 @@ fn main() {
         thread::sleep(du);
         timedisp.inclement_time();
         timedisp.shaping_str();
+    }
+}
+
+fn timer_disp(mut m: u32) {
+    let h: u32 = m / 60;
+    m = m % 60;
+
+    let mut timedisp = Displaytime {
+        second: 0,
+        minute: m as i32,
+        hour: h as i32,
+        second_str: "00".to_string(),
+        minute_str: "00".to_string(),
+        hour_str: "00".to_string(),
+    };
+    let du = Duration::new(1, 0);
+    loop {
+        timedisp.shaping_str();
+        timedisp.display();
+        thread::sleep(du);
+        if timedisp.declement_time() {
+            break;
+        }
+    }
+    println!("!!! finish !!!");
+}
+
+fn main() {
+    let timer_flg: bool;
+    let stopwatch_flg: bool;
+    let end_flg: bool;
+    let timer_minute: u32;
+
+    (timer_flg, stopwatch_flg, end_flg, timer_minute) = option_select();
+
+    if !end_flg {
+        if timer_flg {
+            timer_disp(timer_minute);
+        } else if stopwatch_flg {
+            stopwatch_disp();
+        }
     }
 }
